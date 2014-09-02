89188263ba92c9cef65e66203d0edcd6aa869ce2
patrick <patrickekkel@gmail.com>
2014-09-03 01:08:11 +0200

    configuration is now simplified to localmachine instead of using a vm for rails stuff

--- Java/NationBuilderLib/src/nationbuilder/lib/Ruby/RubyConfiguration.java
+++ Java/NationBuilderLib/src/nationbuilder/lib/Ruby/RubyConfiguration.java
@@ -5,6 +5,6 @@ package nationbuilder.lib.Ruby;
  */
 public class RubyConfiguration {
 
-    public static String RubyBackend = "http://192.168.1.6";
+    public static String RubyBackend = "http://localhost";
     public static String RubyBackendPort = "3000";
 }
