9ad9cb1b532024eae8f8604dd4e6ddd16217a386
Patrick Ekkel <patrickekkel@gmail.com>
2015-05-26 18:31:03 +0200

    NB-24: wip

--- Java/NationBuilderLib/src/nationbuilder/lib/data/map/xml/Configuration.java
+++ Java/NationBuilderLib/src/nationbuilder/lib/data/map/xml/Configuration.java
@@ -11,7 +11,7 @@ public class Configuration {
 	//public static String SmallDemoMap = "/home/patrick/Thuis/Git/nationbuilder/Tiled/Maps/demo_small.tmx";
     // Thuis configuratie
 	public static String SmallDemoMap = "/home/patrick/Git/nationbuilder/Tiled/Maps/demo_small.tmx";
-	public static String NB14Map = "/home/patrick/Thuis/Git/nationbuilder/Tiled/Maps/NB14.tmx";
+	public static String NB14Map = "/home/patrick /Git/nationbuilder/Tiled/Maps/NB14.tmx";
     public static String ChunkingDemo = "/home/patrick/Git/nationbuilder/Tiled/Maps/demo_chunking.tmx";
 	public static String MultilayerChunkingLayer = "/home/patrick/Git/nationbuilder/Tiled/Maps/demo_chunking_multilayer.tmx";
 
