4efae5705be2c3ee3437af461db77039492ce1db
patrick.ekkel <patrick.ekkel@topicus.nl>
2016-02-25 09:29:39 +0100

    NB-13: power_relay_station wordt nu ook goed opgeslagen

--- Java/MapConverter/NationBuilderMapConverter/src/Main.java
+++ Java/MapConverter/NationBuilderMapConverter/src/Main.java
@@ -1,10 +1,4 @@
 import java.io.IOException;
-
-<<<<<<< 879453dba87bc81b79f95d346116bc3e60b86115
-=======
-
->>>>>>> NB-13: tableperclass werkt nu zoals het zou moeten
-import java.io.IOException;
 import World.WorldLoader;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -20,9 +14,7 @@ public class Main {
     public static void main(String[] args) throws IOException, RubyException {
 
         long startTime  = System.currentTimeMillis();
-
         RubyContext context = new RubyContextFactory().createRubyContext(RubyContextType.BULK_INSERT_SQL_JSON_UPDATE_DELETE_SELECT);
-
         WorldLoader worldLoader = new WorldLoader(context);
         worldLoader.Run();
 
--- Java/NationBuilderLib/src/nationbuilder/lib/data/map/entities/Building.java
+++ Java/NationBuilderLib/src/nationbuilder/lib/data/map/entities/Building.java
@@ -1,22 +1,31 @@
 package nationbuilder.lib.data.map.entities;
 
+import nationbuilder.lib.Ruby.Association.annotation.Entity;
+import nationbuilder.lib.Ruby.Association.annotation.ID;
+import nationbuilder.lib.Ruby.Association.annotation.InhiritanceStrategy;
 import nationbuilder.lib.Ruby.Association.annotation.ManyToOne;
 import nationbuilder.lib.Ruby.Association.annotation.Transient;
+import nationbuilder.lib.Ruby.orm.BaseRubyModel;
+import nationbuilder.lib.Ruby.orm.ReferenceMapping;
 
 /**
  * Created by patrick on 9/18/14.
  */
-
-public class Building extends StaticEntity
+@Entity(tableName = "buildings",strategy = InhiritanceStrategy.TablePerClass)
+// TODO: StaticEntity heeft geen database entry, dit moeten we fixen of het concept "Static Entity" laten vallen
+//public class Building extends StaticEntity
+public class Building extends BaseRubyModel
 {
     @Transient
 	private String geo;
     // TODO: dit transient gemaakt, maar dit is een workaround. superclassing en subclassing moeten we later uitwerken
-    @Transient
-	private String name;
+    private String name;
 	//private Contract contract;
 	@ManyToOne(mapIdTo = "geo")
 	private GameEntity owner;
+
+	@ID(mapIdToEntity = PowerRelayStation.class)
+	private ReferenceMapping power_relay_station;
 
 	public String getName()
 	{
@@ -37,14 +46,4 @@ public class Building extends StaticEntity
 	{
 		this.owner = owner;
 	}
-
-//	public Contract getContract()
-	//{
-	//	return contract;
-	//}
-
-	//public void setContract(Contract contract)
-	//{
-	//	this.contract = contract;
-	//}
 }
