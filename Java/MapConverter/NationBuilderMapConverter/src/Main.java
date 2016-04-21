ec4691465066a24c9abc8cd7b87d9cc56d7c8086
patrick.ekkel <patrick.ekkel@topicus.nl>
2016-04-21 09:35:31 +0200

    NB-46: wat rotzooi code weggegooid

--- Java/MapConverter/NationBuilderMapConverter/src/Main.java
+++ Java/MapConverter/NationBuilderMapConverter/src/Main.java
@@ -24,29 +24,10 @@ public class Main {
         long startTime  = System.currentTimeMillis();
 
         RubyContext context = new RubyContextFactory().createRubyContext(RubyContextType.BULK_INSERT_SQL_JSON_UPDATE_DELETE_SELECT);
-        //RubyContext context = new RubyContextFactory().createRubyContext(RubyContextType.JSON);
         WorldLoader worldLoader = new WorldLoader(context);
         worldLoader.Run();
 
-       /* MapImageFile mapImageFile = context.createRubyModel(MapImageFile.class);
-        mapImageFile.setResource(new File("/home/patrick/Git/nationbuilder/Tiled/TileSets/World3.png"));
 
-        MapMap mapMap = context.createRubyModel(MapMap.class);
-        mapMap.setTileHeight(32);
-        mapMap.setTileWidth(32);
-
-        Image image = context.createRubyModel(Image.class);
-        image.setName("imagename");
-        image.setTileHeight(32);
-        image.setTileWidth(32);
-        image.setUrl("http://localhost");
-        image.setImageFile(mapImageFile);
-        image.setMap(mapMap);*/
-
-
-       /* mapMap.Save("/maps/");
-        mapImageFile.Save("/uploads/");
-        image.Save("/images/"); */
         long endtime = System.currentTimeMillis();
 
         Date date = new Date((endtime - startTime));
